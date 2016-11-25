d48bc2885ad57ff0a2e1e5c7b9dc8fe1f462d94c
Mik-chan <mikumikuareka@gmail.com>
2016-11-25 22:48:24 +0200

    Initial Commit

---
+++ Underliner/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Underliner
+//
+//  Created by George Endo on 11/25/16.
+//  Copyright © 2016 Peka co. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Underliner/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Underliner
+//
+//  Created by George Endo on 11/25/16.
+//  Copyright © 2016 Peka co. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
