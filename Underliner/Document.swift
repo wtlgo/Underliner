df81bedcccb13db1be8f27b40c34450342feed8b
wtlgo <georgeendo0@gmail.com>
2019-06-10 04:45:58 +0300

    Initial commit

--- Underliner/Document.swift
+++ Underliner/Document.swift
@@ -27,16 +27,23 @@ class Document: NSDocument {
     }
 
     override func data(ofType typeName: String) throws -> Data {
+        let vc:ViewController = self.windowControllers[0].contentViewController as! ViewController
+        return vc.GetString().data(using: String.Encoding.utf8) ?? Data()
         // Insert code here to write your document to data of the specified type. If outError != nil, ensure that you create and set an appropriate error when returning nil.
         // You can also choose to override fileWrapperOfType:error:, writeToURL:ofType:error:, or writeToURL:ofType:forSaveOperation:originalContentsURL:error: instead.
-        throw NSError(domain: NSOSStatusErrorDomain, code: unimpErr, userInfo: nil)
+        //throw NSError(domain: NSOSStatusErrorDomain, code: unimpErr, userInfo: nil)
     }
 
     override func read(from data: Data, ofType typeName: String) throws {
+        let vc:ViewController = self.windowControllers[0].contentViewController as! ViewController
+        if let text = String(data: data, encoding: String.Encoding.utf8){
+            vc.SetText(text)
+        }
+        //return;
         // Insert code here to read your document from the given data of the specified type. If outError != nil, ensure that you create and set an appropriate error when returning false.
         // You can also choose to override readFromFileWrapper:ofType:error: or readFromURL:ofType:error: instead.
         // If you override either of these, you should also override -isEntireFileLoaded to return false if the contents are lazily loaded.
-        throw NSError(domain: NSOSStatusErrorDomain, code: unimpErr, userInfo: nil)
+        //throw NSError(domain: NSOSStatusErrorDomain, code: unimpErr, userInfo: nil)
     }
 
 
--- Underliner/ViewController.swift
+++ Underliner/ViewController.swift
@@ -8,8 +8,25 @@
 
 import Cocoa
 
+
 class ViewController: NSViewController {
 
+    @IBOutlet var TextToEdit: NSScrollView!
+    @IBOutlet var TextToRemove: NSScrollView!
+    @IBOutlet var FontCheck: NSButtonCell!
+    @IBOutlet var WidthCheck: NSButtonCell!
+    @IBOutlet var WtfCheck: NSButtonCell!
+    
+    public func SetText(_ Text : String){
+        let tv:NSTextView = TextToEdit.documentView! as! NSTextView
+        tv.textStorage?.mutableString.setString(Text)
+    }
+    
+    public func GetString() -> String{
+        let tv:NSTextView = TextToEdit.documentView! as! NSTextView
+        return tv.string!
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -20,6 +37,43 @@ class ViewController: NSViewController {
         didSet {
         // Update the view, if already loaded.
         }
+    }
+    
+    
+    private func ChangeTo_(_ Text : String, _ Find : String) -> String{
+        if Find == ""{
+            return Text
+        }
+        
+        var _ces = ""
+        
+        for _ in 1...Find.characters.count{
+            _ces = _ces + "_"
+        }
+        
+        let opts:String.CompareOptions = String.CompareOptions(rawValue: UInt(FontCheck.state)*String.CompareOptions.caseInsensitive.rawValue | UInt(WidthCheck.state)*String.CompareOptions.widthInsensitive.rawValue | UInt(WtfCheck.state)*String.CompareOptions.diacriticInsensitive.rawValue)
+        
+        return Text.replacingOccurrences(of: Find, with: _ces, options: opts)
+        
+    }
+    
+    
+    @IBAction func ChangeText(_ sender: NSButton) {
+        let TextToEditDocumentView:NSTextView = TextToEdit.documentView! as! NSTextView
+        let TextToRemoveDocumentView:NSTextView = TextToRemove.documentView! as! NSTextView
+        
+        let TextToEditMutableString = TextToEditDocumentView.textStorage?.mutableString
+        
+        var TextToEditStringValue = TextToEditDocumentView.string
+        let TextToRemoveValue:String = TextToRemoveDocumentView.string!
+        
+        let WordsToRemove = TextToRemoveValue.components(separatedBy: "\n")
+        
+        for e in WordsToRemove{
+            TextToEditStringValue = ChangeTo_(TextToEditStringValue!, e)
+        }
+        
+        TextToEditMutableString?.setString(TextToEditStringValue!)
     }
 
 
